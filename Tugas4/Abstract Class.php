f017f3ea7abaf923855b3d53dade181e1c548be2
yogardi <130739722+yogardi@users.noreply.github.com>
2023-05-01 11:42:08 +0700

    Add files via upload

---
+++ Tugas4/Abstract Class.php
@@ -0,0 +1,41 @@
+<?php
+abstract class Peserta {
+abstract protected function dataDiri();
+abstract protected function dataOrtu();
+abstract protected function dataSekolahAsal();
+}
+class Siswa extends Peserta {
+public string $nama = "Yogardi";
+public function dataDiri() {
+return $this->nama;
+}
+
+public function dataOrtu() {
+//
+}
+
+public function dataSekolahAsal() {
+//
+}
+
+}
+class Mahasiswa extends Peserta {
+public string $nama = "Yogardi";
+public function dataDiri() {
+return $this->nama;
+}
+
+public function dataOrtu() {
+//
+}
+
+public function dataSekolahAsal() {
+//
+}
+}
+
+$ssw = new Siswa;
+echo "<p>" . $ssw->dataDiri() . "</p>";
+
+$mhsw = new Mahasiswa;
+echo "<p>" . $mhsw->dataDiri() . "</p>";
---
+++ Tugas4/Interface.php
@@ -0,0 +1,41 @@
+<?php
+interface Peserta {
+public function dataDiri();
+public function dataOrtu();
+public function dataSekolahAsal();
+}
+class Siswa implements Peserta {
+public string $nama = "Yogardi";
+public function dataDiri() {
+return $this->nama;
+}
+
+public function dataOrtu() {
+//
+}
+
+public function dataSekolahAsal() {
+//
+}
+
+}
+class Mahasiswa implements Peserta {
+public string $nama = "Yogardi";
+public function dataDiri() {
+return $this->nama;
+}
+
+
+public function dataOrtu() {
+//
+}
+
+public function dataSekolahAsal() {
+//
+}
+}
+
+$ssw = new Siswa;
+echo "<p>" . $ssw->dataDiri() . "</p>";
+$mhsw = new Mahasiswa;
+echo "<p>" . $mhsw->dataDiri() . "</p>";
